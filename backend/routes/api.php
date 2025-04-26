4b31604289f8e3174f1ff5fed698bb99324d50c4
ChromaHuN <levente547@gmail.com>
2025-04-26 20:18:13 +0200

    Profile liked recipes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,6 +11,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::middleware('auth:sanctum')->get('/user/recipes', [RecipeController::class, 'userRecipes']);
 
+Route::middleware('auth:sanctum')->get('/user/liked-recipes', [RecipeController::class, 'likedRecipes']);
+
+Route::middleware('auth:sanctum')->post('/recipes/{id}/like', [RecipeLikeController::class, 'store']);
 
 Route::get('/recipes', [RecipeController::class, 'index']);
 Route::get('/recipes/{id}', [RecipeController::class, 'show']);
