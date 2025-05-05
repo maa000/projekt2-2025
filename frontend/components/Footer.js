641a80bf7b900fa529bcf79fc236b30c0f503412
balintp105 <balintp105@gmail.com>
2025-05-05 18:36:07 +0200

    header, footer rewrite

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -9,27 +9,30 @@ export default function Footer() {
                 <div>
                     <h3 className="text-xl font-bold">Tastyfiee</h3>
                     <div className="flex gap-2 mt-2">
-                        <a href="#">📷</a>
-                        <a href="#">📘</a>
-                        <a href="#">📺</a>
-                        <a href="#">📸</a>
+                        {/*<a href="#">📷</a>*/}
+                        {/*<a href="#">📘</a>*/}
+                        {/*<a href="#">📺</a>*/}
+                        {/*<a href="#">📸</a>*/}
                     </div>
                 </div>
                 <div className="space-y-1">
-                    <p>Contact</p>
-                    <p>Support</p>
-                    <p>Privacy</p>
+                    <p className="text-xl font-semibold">Contact</p>
+                    {/*<p>Support</p>*/}
+                    {/*<p>Privacy</p>*/}
+                    <Link href="/team" className="hover:underline"><p>Team</p></Link>
                     <Link href="/terms" className="hover:underline"><p>Terms</p></Link>
                 </div>
+                {/*<div className="space-y-1">*/}
+                {/*    <p>Kategóriák</p>*/}
+                {/*    <p>Felkapott receptek</p>*/}
+                {/*    <p>Legjobbak</p>*/}
+                {/*</div>*/}
                 <div className="space-y-1">
-                    <p>Kategóriák</p>
-                    <p>Felkapott receptek</p>
-                    <p>Legjobbak</p>
-                </div>
-                <div className="space-y-1">
-                    <p>Site Links</p>
-                    <p>Team</p>
-                    <p>Log in</p>
+                    <p className="text-xl font-semibold">Site Links</p>
+                    {/*<p>Team</p>*/}
+                    {/*<p>Log in</p>*/}
+                    <Link href="/login" className="hover:underline"><p>Log in</p></Link>
+                    <Link href="/register" className="hover:underline"><p>Register</p></Link>
                 </div>
             </div>
         </footer>
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -13,26 +13,26 @@ export default function Header() {
     }, [])
 
     return (
-        <header className="bg-gray-900 text-white p-8 flex justify-between">
+        <header className="bg-gray-900 text-white p-8 h-30 items-center flex justify-between">
             <div className="text-xl font-bold">
-                <Link href="/">Tastyfiee</Link>
+                <Link href="/" className="text-3xl">Tastyfiee</Link>
             </div>
             <nav className="space-x-4">
-                <Link href="/">Kezdőlap</Link>
-                <Link href="/recipes">Receptek</Link>
+                <Link href="/" className="text-xl">Kezdőlap</Link>
+                <Link href="/recipes" className="text-xl">Receptek</Link>
 
                 {isClient && user ? (
                     <>
-                        <Link href="/recept-bekuldese">Recept beküldése</Link>
-                        <Link href="/profil">Profil</Link>
-                        <button onClick={logout} className="text-red-400 ml-2">
+                        <Link href="/recept-bekuldese" className="text-xl">Recept beküldése</Link>
+                        <Link href="/profil" className="text-xl">Profil</Link>
+                        <button onClick={logout} className="text-red-400 text-xl ml-2">
                             Kilépés
                         </button>
                     </>
                 ) : (
                     <>
-                        <Link href="/login">Belépés</Link>
-                        <Link href="/register">Regisztráció</Link>
+                        <Link href="/login" className="text-xl">Belépés</Link>
+                        <Link href="/register" className="text-xl">Regisztráció</Link>
                     </>
                 )}
             </nav>
